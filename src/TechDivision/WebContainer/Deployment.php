760e5b8979be0b84740564a26d17ddaa73e8039d
Johann Zelger <zelger@me.com>
2014-03-04 10:59:40 +0100

    refactored name to WebContainer

--- src/TechDivision/WebContainer/Deployment.php
+++ src/TechDivision/WebContainer/Deployment.php
@@ -1,18 +1,18 @@
 <?php
 /**
- * TechDivision\WebServerContainer\Deployment
+ * TechDivision\WebContainer\Deployment
  *
  * PHP version 5
  *
  * @category  Appserver
- * @package   TechDivision_WebServerContainer
+ * @package   TechDivision_WebContainer
  * @author    Johann Zelger <jz@techdivision.com>
  * @copyright 2014 TechDivision GmbH <info@techdivision.com>
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link      http://www.appserver.io
  */
 
-namespace TechDivision\WebServerContainer;
+namespace TechDivision\WebContainer;
 
 use TechDivision\ApplicationServer\AbstractDeployment;
 
@@ -20,7 +20,7 @@ use TechDivision\ApplicationServer\AbstractDeployment;
  * Class Deployment
  *
  * @category  Appserver
- * @package   TechDivision_WebServerContainer
+ * @package   TechDivision_WebContainer
  * @author    Johann Zelger <jz@techdivision.com>
  * @copyright 2014 TechDivision GmbH <info@techdivision.com>
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
--- src/TechDivision/WebContainer/ServerNodeConfiguration.php
+++ src/TechDivision/WebContainer/ServerNodeConfiguration.php
@@ -1,18 +1,18 @@
 <?php
 /**
- * \TechDivision\WebServerContainer\ServerNodeConfiguration
+ * \TechDivision\WebContainer\ServerNodeConfiguration
  *
  * PHP version 5
  *
  * @category  Appserver
- * @package   TechDivision_WebServerContainer
+ * @package   TechDivision_WebContainer
  * @author    Johann Zelger <jz@techdivision.com>
  * @copyright 2014 TechDivision GmbH <info@techdivision.com>
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link      http://www.appserver.io
  */
 
-namespace TechDivision\WebServerContainer;
+namespace TechDivision\WebContainer;
 
 use TechDivision\ApplicationServer\Api\Node\NodeInterface;
 use TechDivision\WebServer\Interfaces\ServerConfigurationInterface;
@@ -21,7 +21,7 @@ use TechDivision\WebServer\Interfaces\ServerConfigurationInterface;
  * Class ServerNodeConfiguration
  *
  * @category  Appserver
- * @package   TechDivision_WebServerContainer
+ * @package   TechDivision_WebContainer
  * @author    Johann Zelger <jz@techdivision.com>
  * @copyright 2014 TechDivision GmbH <info@techdivision.com>
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
