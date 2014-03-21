0f48077677f64764341c29827edff2602f5f227d
wickb <b.wick@techdivision.com>
2014-03-21 10:44:36 +0100

    refactoring to latest interface changes

--- src/TechDivision/WebContainer/ServerNodeConfiguration.php
+++ src/TechDivision/WebContainer/ServerNodeConfiguration.php
@@ -351,19 +351,4 @@ class ServerNodeConfiguration implements ServerConfigurationInterface
         // return the rewrites
         return $this->rewrites;
     }
-
-    /**
-     * Will prepend a given array of rewrite arrays to the global rewrite pool.
-     * Rewrites arrays have to be the form of array('condition' => ..., 'target' => ..., 'flag' => ...)
-     *
-     * @param array $rewriteArrays The array of rewrite arrays(!) to prepend
-     *
-     * @return boolean
-     */
-    public function prependRewriteArrays(array $rewriteArrays)
-    {
-        $this->rewrites = array_merge($rewriteArrays, $this->rewrites);
-
-        return (bool) $this->rewrites;
-    }
 }
